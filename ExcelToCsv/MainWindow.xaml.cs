f7e2203e1d4ebfce94df166bb01fa1ba59ed4fc4
Sergey Petrov <petrov@comfortlab.ru>
2019-01-26 22:54:27 +0700

    Signed-off-by: Sergey Petrov <petrov@comfortlab.ru>

---
+++ ExcelToCsv/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace ExcelToCsv
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new ViewModel();
+        }
+    }
+}
---
+++ ExcelToCsv/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ExcelToCsv.Annotations;
+
+namespace ExcelToCsv
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
